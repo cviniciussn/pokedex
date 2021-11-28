251cedc0d8f39e7a91f61fd3a123261377f89b35
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-11-28 10:41:51 -0300

    hedadr

---
+++ src/components/Header/header-style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Header = styled.header`
+
+`;
+
+export {
+    Header,
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import styled from 'styled-components'
-import PokeCard from '../components/Cards/Cards'
+import React from 'react';
+import styled from 'styled-components';
+// import PokeCard from '../components/Cards/Cards';
+import Header from '../components/Header/Header';
 
 const HomeComponent = styled.div`
 
     width: 100vw;
     height: 100vh;
 
-    display: flex;
+    ${'' /* display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
-
-   //background-color: blue;
-
+    justify-content: center; 
+        comentado para não ficar no centro.
+    */}
 `
 
 export default function Home() {
     return (
         <HomeComponent>
-            <PokeCard></PokeCard>
+            <Header></Header>
+            {/* <PokeCard></PokeCard> */}
         </HomeComponent>
     )
 }
