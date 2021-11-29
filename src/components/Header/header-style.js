ebd8a1357cc6bdeaabcb95229d1a10ad2cffbfd8
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-11-29 14:01:36 -0300

    retira estilo do mesmo arquivo do componente

--- src/components/Header/header-style.js
+++ src/components/Header/header-style.js
@@ -1,9 +1,63 @@
 import styled from 'styled-components'
 
-const Header = styled.header`
+
+const HeaderComponent = styled.header`
+background-color: #F5DB13;
+padding: 16px 120px;
+position: relative;
+box-shadow: 0px 4px 16px rgba(1, 28, 64, 0.2);
+
+    .header-content {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        
+
+        > img {
+            width: 160px;
+            height: 63px;
+        }
+
+        .navigation {
+            list-style-type: none;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            li {
+
+              > a {
+                    text-decoration: none;
+                    padding: 10px;
+                    color: #000;
+                }
+
+                &.active{
+                    position: relative;
+                    &:after {
+                        content: '';
+                        width: 62px;
+                        height: 3px;
+                        display: block;
+                        border-radius: 12px;
+                        background-color: #000;
+                        position: absolute;
+                        bottom: -12px;
+                        left: 50%;
+                        transform: translate3d(-50%,0,0);
+                    }
+                }
+            }
+
+            li + li {
+                margin-left: 25px;
+            }
+        }
+    }
 
 `;
 
 export {
-    Header,
+    HeaderComponent,
 };
