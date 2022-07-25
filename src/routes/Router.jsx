19e42584cc7707ad8a52671b8c18e4208510c135
cviniciussn <cvinicius@outlook.com>
2022-07-25 19:04:18 -0300

    minor fix to routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Pokedex from '../pages/Pokedex/Pokedex'
 import Home from '../pages/Home/Home'
 
@@ -7,8 +7,10 @@ import Home from '../pages/Home/Home'
 export default function Router() {
     return (
         <BrowserRouter>
-            <Route path='/' component={Home} exact />
-            <Route path='/pokedex' component={Pokedex} exact />
+            <Routes>
+                <Route path='/' element={<Home />} exact />
+                <Route path='/pokedex' element={<Pokedex />} exact />
+            </Routes>
         </BrowserRouter>
     )
 }
